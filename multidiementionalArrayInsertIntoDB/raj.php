13cea76aab670a0021c1cb31f670c77683ab06b1
Harsh Doshi <68558629+HD-Harsh-Doshi@users.noreply.github.com>
2020-10-01 14:28:50 +0530

    Update raj.php

--- multidiementionalArrayInsertIntoDB/raj.php
+++ multidiementionalArrayInsertIntoDB/raj.php
@@ -10,13 +10,13 @@ $password = "1234";
 $dbname = "wordappz";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+   die("Can't Connect. Connection Error: " . mysqli_connect_errno()." ".mysqli_connect_error());  
 }
 
-$sql = "show tables;";
+$sql = "select * from tables";
 $data = $conn->query($sql);
 
 
